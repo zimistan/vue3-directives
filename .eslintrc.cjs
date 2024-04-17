f78566867ea74bb17638e74f3d8ff06f326092fe
adiljan <a__dil@outlook.com>
2024-04-18 01:01:09 +0800

    Complete basic functionality for autoScroll
    Detailed description:
    - Implemented the basic scrolling logic for autoScroll

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,7 +28,7 @@ module.exports = {
   },
   plugins: ["@typescript-eslint", "vue", "prettier"],
   rules: {
-    indent: ["error", 2],
+    indent: ["error", 2, { "SwitchCase": 1 }],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "never"],

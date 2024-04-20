d73cff7ccc1aedcfb44cabfd67fe8912c845d04f
adiljan <a__dil@outlook.com>
2024-04-20 20:20:01 +0800

    add eventOutside

--- src/directives/event-outside/index.ts
+++ src/directives/event-outside/index.ts
@@ -1,4 +1,4 @@
-import { Directive, DirectiveBinding } from "vue"
+import { ObjectDirective } from "vue"
 
 type Node = {
   element: Element
@@ -6,27 +6,55 @@ type Node = {
   event: () => void
 }
 
-function updateInfo(el: HTMLElement, binding: DirectiveBinding) {
-  nodes.set(el, {
-    element: el,
-    eventType: binding.arg,
-    event: binding.value,
-  })
+interface DocumentMouseEvents {
+  auxclick: Map<HTMLElement, Node>
+  click: Map<HTMLElement, Node>
+  contextmenu: Map<HTMLElement, Node>
+  dblclick: Map<HTMLElement, Node>
+  mousedown: Map<HTMLElement, Node>
+  mouseenter: Map<HTMLElement, Node>
+  mouseleave: Map<HTMLElement, Node>
+  mousemove: Map<HTMLElement, Node>
+  mouseover: Map<HTMLElement, Node>
+  mouseout: Map<HTMLElement, Node>
+  mouseup: Map<HTMLElement, Node>
 }
 
-function handleEventOutside(node: Node, event: Function) {
-  if (node.element.contains(event.target) && node.element !== event.target) {
-  }
-}
+type DocumentMouseEventsKeys = keyof DocumentMouseEvents
 
-const nodes: Map<Element, Node> = new Map()
-const eventOutside: Directive = {
+const documentMouseEvents: DocumentMouseEvents = {
+  auxclick: new Map(),
+  click: new Map(),
+  contextmenu: new Map(),
+  dblclick: new Map(),
+  mousedown: new Map(),
+  mouseenter: new Map(),
+  mouseleave: new Map(),
+  mousemove: new Map(),
+  mouseover: new Map(),
+  mouseout: new Map(),
+  mouseup: new Map(),
+}
+const eventOutside: ObjectDirective = {
   mounted: (el, binding) => {
-    updateInfo(el, binding)
-    el.addEventListener(binding.arg, handleEventOutside(nodes.get(el) as Node, binding.value))
-  },
-  updated: (el, binding) => {
-    updateInfo(el, binding)
+    if (binding.arg) {
+      // 确保 binding.arg 是 DocumentMouseEvents 的一个键
+      if (!((binding.arg as DocumentMouseEventsKeys) in documentMouseEvents)) {
+        throw new Error("必须传递一个有效的事件名称")
+      }
+    }
+    if (documentMouseEvents[binding.arg as DocumentMouseEventsKeys].size === 0) {
+      document.addEventListener(binding.arg as DocumentMouseEventsKeys, (event) => {
+        console.log(event)
+      })
+    }
+    if (!documentMouseEvents[binding.arg as DocumentMouseEventsKeys].has(el)) {
+      documentMouseEvents[binding.arg as DocumentMouseEventsKeys].set(el, {
+        element: el,
+        eventType: binding.arg,
+        event: binding.value,
+      })
+    }
   },
 }
 
--- src/test/eventOutside.vue
+++ src/test/eventOutside.vue
@@ -1,12 +1,18 @@
 <script setup>
-function seyHello() {
-  console.log("你好")
+function seyHello(text, h) {
+  console.log(text, h)
 }
+
+document.addEventListener("click", seyHello.bind(this, "hahahaha"))
+setTimeout(() => {
+  document.removeEventListener("click", seyHello)
+  console.log("time out")
+}, 5000)
 </script>
 
 <template>
   <div>
-    <button v-eventOutside:click="seyHello">点击我外面即可触发</button>
+    <button>点击我外面即可触发</button>
   </div>
 </template>
 

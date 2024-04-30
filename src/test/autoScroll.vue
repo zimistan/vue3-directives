940f3335cb01e75113c4f7fcbd3529468ceddfec
adiljan <a__dil@outlook.com>
2024-04-30 10:01:24 +0800

    Modify configuration

--- src/test/autoScroll.vue
+++ src/test/autoScroll.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import { ref } from "vue"
+import { AutoScrollOption } from "../types/optionTypes/index"
 
 let num = ref(100)
+const autoScrollOption = ref<AutoScrollOption>({
+  speed: 1,
+  backSpeed: 30,
+})
 </script>
 
 <template>
   <div class="flex gap-xl">
-    <ul v-autoScroll:BOTTOM="true" class="bg-red w-50 h-xl overflow-y-scroll">
+    <ul v-autoScroll:BOTTOM="autoScrollOption" class="bg-red w-50 h-xl overflow-y-scroll">
       <li class="m-r" v-for="item in num" :key="item">{{ item }}</li>
     </ul>
     <button @click="num++">点击</button>

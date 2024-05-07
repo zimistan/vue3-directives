bd724b94e1b25555d23074a2d6a366b69159e9ae
adiljan <a__dil@outlook.com>
2024-05-08 00:09:37 +0800

    modify option

--- src/test/autoScroll.vue
+++ src/test/autoScroll.vue
@@ -7,14 +7,23 @@ const autoScrollOption = ref<AutoScrollOption>({
   speed: 1,
   backSpeed: 30,
 })
+
+const dicoration = ref("BOTTOM")
+
+function onClick() {
+  autoScrollOption.value = {
+    speed: 2,
+    backSpeed: 30,
+  }
+}
 </script>
 
 <template>
   <div class="flex gap-xl">
-    <ul v-autoScroll:BOTTOM="autoScrollOption" class="bg-red w-50 h-xl overflow-y-scroll">
+    <ul v-autoScroll:[dicoration]="autoScrollOption" class="bg-red w-50 h-xl overflow-y-scroll">
       <li class="m-r" v-for="item in num" :key="item">{{ item }}</li>
     </ul>
-    <button @click="num++">点击</button>
+    <button @click="onClick">点击</button>
   </div>
 </template>
 
